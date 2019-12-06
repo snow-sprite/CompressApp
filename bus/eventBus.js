2a1efc7959864e7fe55a8aca5dd0016353e16835
zhouning <zhouning@jinse.com>
2019-12-07 00:06:46 +0800

    feat: add zip-local

--- bus/eventBus.js
+++ bus/eventBus.js
@@ -5,16 +5,17 @@ const {
 const tinify = require('tinify')
 const fs = require('fs')
 const path = require('path')
-
+const zipper = require('zip-local')
 const API_KEY = "fvDPnGNpDZRJsrtR5KdM4Qcbp8RvcYhN"
 tinify.key = API_KEY
 
-
+// 源文件夹
 var sourcePath = ''
+// 目标文件夹
 var targetPath = ''
 ipcMain.on('dragEventMessage', function(event, fPath) {
   sourcePath = fPath
-  targetPath = path.join(`${fPath}-compresed`);
+  targetPath = path.resolve(`${fPath}-compresed`);
   compresePic()
   // readFPath(fPath)
   // event.reply('dragEventReply', arg)
@@ -35,6 +36,23 @@ function readFPath(fPath) {
           `${targetPath}/${minName.split(".")[0]}.min.${minName.split(".")[1]}`,
           () => {
             console.log('success!')
+            // TODO sync压缩
+            setTimeout(function() {
+              console.log('开始压缩')
+              console.log("目标目录", targetPath);
+              let targetPathArr = targetPath.split('/')
+              let targetFileName = targetPathArr[targetPathArr.length - 1]
+              let newTargetPath = targetPath.replace(
+                targetPathArr[targetPathArr.length - 1], ''
+              )
+              zipper.sync
+                .zip(targetPath)
+                .compress()
+                .save(`${newTargetPath}${targetFileName}.zip`, function() {
+                  console.log("压缩成功哦");
+                  // TODO 打完压缩包可以不用生成文件夹了
+                });
+            }, 30000)
           }
         );
     } else if (stat.isDirectory()) {
