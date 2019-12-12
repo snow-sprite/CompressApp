2b970b98321dab6e81d4eba65262ba6953734c1f
zhouning <zhouning@jinse.com>
2019-12-12 17:58:40 +0800

    feat: 重构

--- dist/electron/main.js
+++ dist/electron/main.js
@@ -102,10 +102,10 @@ eval("/* WEBPACK VAR INJECTION */(function(__dirname) {var resolve = __webpack_r
 /*!****************************************!*\
   !*** ./node_modules/7zip/package.json ***!
   \****************************************/
-/*! exports provided: _from, _id, _inBundle, _integrity, _location, _phantomChildren, _requested, _requiredBy, _resolved, _shasum, _spec, _where, bin, bugs, bundleDependencies, deprecated, description, homepage, keywords, license, main, name, repository, scripts, version, default */
+/*! exports provided: name, version, description, keywords, repository, bin, main, scripts, license, default */
 /***/ (function(module) {
 
-eval("module.exports = JSON.parse(\"{\\\"_from\\\":\\\"7zip@0.0.6\\\",\\\"_id\\\":\\\"7zip@0.0.6\\\",\\\"_inBundle\\\":false,\\\"_integrity\\\":\\\"sha1-nK+xca+CMpSQNTtIFvAzR6oVCjA=\\\",\\\"_location\\\":\\\"/7zip\\\",\\\"_phantomChildren\\\":{},\\\"_requested\\\":{\\\"type\\\":\\\"version\\\",\\\"registry\\\":true,\\\"raw\\\":\\\"7zip@0.0.6\\\",\\\"name\\\":\\\"7zip\\\",\\\"escapedName\\\":\\\"7zip\\\",\\\"rawSpec\\\":\\\"0.0.6\\\",\\\"saveSpec\\\":null,\\\"fetchSpec\\\":\\\"0.0.6\\\"},\\\"_requiredBy\\\":[\\\"/electron-devtools-installer\\\"],\\\"_resolved\\\":\\\"https://registry.npm.taobao.org/7zip/download/7zip-0.0.6.tgz\\\",\\\"_shasum\\\":\\\"9cafb171af82329490353b4816f03347aa150a30\\\",\\\"_spec\\\":\\\"7zip@0.0.6\\\",\\\"_where\\\":\\\"/Users/ningzhou/workgroup/test/minify_your_images/node_modules/electron-devtools-installer\\\",\\\"bin\\\":{\\\"7z\\\":\\\"7zip-lite/7z.exe\\\"},\\\"bugs\\\":{\\\"url\\\":\\\"https://github.com/fritx/win-7zip/issues\\\"},\\\"bundleDependencies\\\":false,\\\"deprecated\\\":false,\\\"description\\\":\\\"7zip Windows Package via Node.js\\\",\\\"homepage\\\":\\\"https://github.com/fritx/win-7zip#readme\\\",\\\"keywords\\\":[\\\"7z\\\",\\\"7zip\\\",\\\"7-zip\\\",\\\"windows\\\",\\\"install\\\"],\\\"license\\\":\\\"GNU LGPL\\\",\\\"main\\\":\\\"index.js\\\",\\\"name\\\":\\\"7zip\\\",\\\"repository\\\":{\\\"type\\\":\\\"git\\\",\\\"url\\\":\\\"git+ssh://git@github.com/fritx/win-7zip.git\\\"},\\\"scripts\\\":{\\\"test\\\":\\\"mocha\\\"},\\\"version\\\":\\\"0.0.6\\\"}\");\n\n//# sourceURL=webpack:///./node_modules/7zip/package.json?");
+eval("module.exports = JSON.parse(\"{\\\"name\\\":\\\"7zip\\\",\\\"version\\\":\\\"0.0.6\\\",\\\"description\\\":\\\"7zip Windows Package via Node.js\\\",\\\"keywords\\\":[\\\"7z\\\",\\\"7zip\\\",\\\"7-zip\\\",\\\"windows\\\",\\\"install\\\"],\\\"repository\\\":\\\"git@github.com:fritx/win-7zip.git\\\",\\\"bin\\\":{\\\"7z\\\":\\\"7zip-lite/7z.exe\\\"},\\\"main\\\":\\\"index.js\\\",\\\"scripts\\\":{\\\"test\\\":\\\"mocha\\\"},\\\"license\\\":\\\"GNU LGPL\\\"}\");\n\n//# sourceURL=webpack:///./node_modules/7zip/package.json?");
 
 /***/ }),
 
@@ -360,6 +360,18 @@ eval("module.exports = globSync\nglobSync.GlobSync = GlobSync\n\nvar fs = __webp
 
 /***/ }),
 
+/***/ "./node_modules/has-flag/index.js":
+/*!****************************************!*\
+  !*** ./node_modules/has-flag/index.js ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\nmodule.exports = (flag, argv) => {\n\targv = argv || process.argv;\n\tconst prefix = flag.startsWith('-') ? '' : (flag.length === 1 ? '-' : '--');\n\tconst pos = argv.indexOf(prefix + flag);\n\tconst terminatorPos = argv.indexOf('--');\n\treturn pos !== -1 && (terminatorPos === -1 ? true : pos < terminatorPos);\n};\n\n\n//# sourceURL=webpack:///./node_modules/has-flag/index.js?");
+
+/***/ }),
+
 /***/ "./node_modules/inflight/inflight.js":
 /*!*******************************************!*\
   !*** ./node_modules/inflight/inflight.js ***!
@@ -480,7 +492,7 @@ eval("exports = module.exports = SemVer\n\nvar debug\n/* istanbul ignore next */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\nvar argv = process.argv;\n\nvar terminator = argv.indexOf('--');\nvar hasFlag = function (flag) {\n\tflag = '--' + flag;\n\tvar pos = argv.indexOf(flag);\n\treturn pos !== -1 && (terminator !== -1 ? pos < terminator : true);\n};\n\nmodule.exports = (function () {\n\tif ('FORCE_COLOR' in process.env) {\n\t\treturn true;\n\t}\n\n\tif (hasFlag('no-color') ||\n\t\thasFlag('no-colors') ||\n\t\thasFlag('color=false')) {\n\t\treturn false;\n\t}\n\n\tif (hasFlag('color') ||\n\t\thasFlag('colors') ||\n\t\thasFlag('color=true') ||\n\t\thasFlag('color=always')) {\n\t\treturn true;\n\t}\n\n\tif (process.stdout && !process.stdout.isTTY) {\n\t\treturn false;\n\t}\n\n\tif (process.platform === 'win32') {\n\t\treturn true;\n\t}\n\n\tif ('COLORTERM' in process.env) {\n\t\treturn true;\n\t}\n\n\tif (process.env.TERM === 'dumb') {\n\t\treturn false;\n\t}\n\n\tif (/^screen|^xterm|^vt100|color|ansi|cygwin|linux/i.test(process.env.TERM)) {\n\t\treturn true;\n\t}\n\n\treturn false;\n})();\n\n\n//# sourceURL=webpack:///./node_modules/supports-color/index.js?");
+eval("\nconst os = __webpack_require__(/*! os */ \"os\");\nconst hasFlag = __webpack_require__(/*! has-flag */ \"./node_modules/has-flag/index.js\");\n\nconst {env} = process;\n\nlet forceColor;\nif (hasFlag('no-color') ||\n\thasFlag('no-colors') ||\n\thasFlag('color=false') ||\n\thasFlag('color=never')) {\n\tforceColor = 0;\n} else if (hasFlag('color') ||\n\thasFlag('colors') ||\n\thasFlag('color=true') ||\n\thasFlag('color=always')) {\n\tforceColor = 1;\n}\nif ('FORCE_COLOR' in env) {\n\tif (env.FORCE_COLOR === true || env.FORCE_COLOR === 'true') {\n\t\tforceColor = 1;\n\t} else if (env.FORCE_COLOR === false || env.FORCE_COLOR === 'false') {\n\t\tforceColor = 0;\n\t} else {\n\t\tforceColor = env.FORCE_COLOR.length === 0 ? 1 : Math.min(parseInt(env.FORCE_COLOR, 10), 3);\n\t}\n}\n\nfunction translateLevel(level) {\n\tif (level === 0) {\n\t\treturn false;\n\t}\n\n\treturn {\n\t\tlevel,\n\t\thasBasic: true,\n\t\thas256: level >= 2,\n\t\thas16m: level >= 3\n\t};\n}\n\nfunction supportsColor(stream) {\n\tif (forceColor === 0) {\n\t\treturn 0;\n\t}\n\n\tif (hasFlag('color=16m') ||\n\t\thasFlag('color=full') ||\n\t\thasFlag('color=truecolor')) {\n\t\treturn 3;\n\t}\n\n\tif (hasFlag('color=256')) {\n\t\treturn 2;\n\t}\n\n\tif (stream && !stream.isTTY && forceColor === undefined) {\n\t\treturn 0;\n\t}\n\n\tconst min = forceColor || 0;\n\n\tif (env.TERM === 'dumb') {\n\t\treturn min;\n\t}\n\n\tif (process.platform === 'win32') {\n\t\t// Node.js 7.5.0 is the first version of Node.js to include a patch to\n\t\t// libuv that enables 256 color output on Windows. Anything earlier and it\n\t\t// won't work. However, here we target Node.js 8 at minimum as it is an LTS\n\t\t// release, and Node.js 7 is not. Windows 10 build 10586 is the first Windows\n\t\t// release that supports 256 colors. Windows 10 build 14931 is the first release\n\t\t// that supports 16m/TrueColor.\n\t\tconst osRelease = os.release().split('.');\n\t\tif (\n\t\t\tNumber(process.versions.node.split('.')[0]) >= 8 &&\n\t\t\tNumber(osRelease[0]) >= 10 &&\n\t\t\tNumber(osRelease[2]) >= 10586\n\t\t) {\n\t\t\treturn Number(osRelease[2]) >= 14931 ? 3 : 2;\n\t\t}\n\n\t\treturn 1;\n\t}\n\n\tif ('CI' in env) {\n\t\tif (['TRAVIS', 'CIRCLECI', 'APPVEYOR', 'GITLAB_CI'].some(sign => sign in env) || env.CI_NAME === 'codeship') {\n\t\t\treturn 1;\n\t\t}\n\n\t\treturn min;\n\t}\n\n\tif ('TEAMCITY_VERSION' in env) {\n\t\treturn /^(9\\.(0*[1-9]\\d*)\\.|\\d{2,}\\.)/.test(env.TEAMCITY_VERSION) ? 1 : 0;\n\t}\n\n\tif (env.COLORTERM === 'truecolor') {\n\t\treturn 3;\n\t}\n\n\tif ('TERM_PROGRAM' in env) {\n\t\tconst version = parseInt((env.TERM_PROGRAM_VERSION || '').split('.')[0], 10);\n\n\t\tswitch (env.TERM_PROGRAM) {\n\t\t\tcase 'iTerm.app':\n\t\t\t\treturn version >= 3 ? 3 : 2;\n\t\t\tcase 'Apple_Terminal':\n\t\t\t\treturn 2;\n\t\t\t// No default\n\t\t}\n\t}\n\n\tif (/-256(color)?$/i.test(env.TERM)) {\n\t\treturn 2;\n\t}\n\n\tif (/^screen|^xterm|^vt100|^vt220|^rxvt|color|ansi|cygwin|linux/i.test(env.TERM)) {\n\t\treturn 1;\n\t}\n\n\tif ('COLORTERM' in env) {\n\t\treturn 1;\n\t}\n\n\treturn min;\n}\n\nfunction getSupportLevel(stream) {\n\tconst level = supportsColor(stream);\n\treturn translateLevel(level);\n}\n\nmodule.exports = {\n\tsupportsColor: getSupportLevel,\n\tstdout: getSupportLevel(process.stdout),\n\tstderr: getSupportLevel(process.stderr)\n};\n\n\n//# sourceURL=webpack:///./node_modules/supports-color/index.js?");
 
 /***/ }),
 
@@ -525,7 +537,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var elec
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("__webpack_require__(/*! /Users/ningzhou/workgroup/test/minify_your_images/src/main/index.dev.js */\"./src/main/index.dev.js\");\nmodule.exports = __webpack_require__(/*! /Users/ningzhou/workgroup/test/minify_your_images/src/main/index.js */\"./src/main/index.js\");\n\n\n//# sourceURL=webpack:///multi_./src/main/index.dev.js_./src/main/index.js?");
+eval("__webpack_require__(/*! /Users/ningzhou/workgroup/test/Electorn/src/main/index.dev.js */\"./src/main/index.dev.js\");\nmodule.exports = __webpack_require__(/*! /Users/ningzhou/workgroup/test/Electorn/src/main/index.js */\"./src/main/index.js\");\n\n\n//# sourceURL=webpack:///multi_./src/main/index.dev.js_./src/main/index.js?");
 
 /***/ }),
 
@@ -592,6 +604,17 @@ eval("module.exports = require(\"fs\");\n\n//# sourceURL=webpack:///external_%22
 /***/ (function(module, exports) {
 
 eval("module.exports = require(\"https\");\n\n//# sourceURL=webpack:///external_%22https%22?");
+
+/***/ }),
+
+/***/ "os":
+/*!*********************!*\
+  !*** external "os" ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"os\");\n\n//# sourceURL=webpack:///external_%22os%22?");
 
 /***/ }),
 
--- src/main/bridge.js
+++ src/main/bridge.js
@@ -1,127 +1,127 @@
-const {
-  ipcMain
-} = require('electron')
+// const {
+//   ipcMain
+// } = require('electron')
 
-const tinify = require('tinify')
-const fs = require('fs')
-const path = require('path')
-const zipper = require('zip-local')
-const API_KEY = "fvDPnGNpDZRJsrtR5KdM4Qcbp8RvcYhN"
-tinify.key = API_KEY
+// const tinify = require('tinify')
+// const fs = require('fs')
+// const path = require('path')
+// const zipper = require('zip-local')
+// const API_KEY = 'fvDPnGNpDZRJsrtR5KdM4Qcbp8RvcYhN'
+// tinify.key = API_KEY
 
-// 源文件夹
-var sourcePath = ''
-// 目标文件夹
-var targetPath = ''
-// 文件数量
-var FILENUM = 0
-// 已压缩文件数
-var FINISHEDFILENUM = 0
+// // 源文件夹
+// var sourcePath = ''
+// // 目标文件夹
+// var targetPath = ''
+// // 文件数量
+// var FILENUM = 0
+// // 已压缩文件数
+// var FINISHEDFILENUM = 0
 
-// 渲染的图片列表
-let renderArr = []
-ipcMain.on('dragEventMessage', function(event, fPath) {
-  sourcePath = fPath
-  targetPath = path.resolve(`${fPath}-compresed`);
-  compresePic(event);
-  // event.reply('dragEventReply', arg)
-})
+// // 渲染的图片列表
+// let renderArr = []
+// ipcMain.on('dragEventMessage', function (event, fPath) {
+//   sourcePath = fPath
+//   targetPath = path.resolve(`${fPath}-compresed`)
+//   compresePic(event)
+//   // event.reply('dragEventReply', arg)
+// })
 
-// 读取文件夹
-function readFPath(fPath, eventReply) {
-  fs.lstat(fPath, function(errs, stat) {
-    if (errs) throw errs;
-    if (stat.isFile()) {
-      FILENUM += 1;
-      //  compressing...
-      let minNameArrLeng = fPath.split("/").length;
-      let minName = fPath.split("/")[minNameArrLeng - 1];
-      renderArr.push({
-        name: minName,
-        path: `${fPath}`
-      })
-      eventReply.reply('filesList', renderArr)
-      // tinypng api
-      // tinify
-      //   .fromFile(path.resolve(fPath))
-      //   .toFile(
-      //     `${targetPath}/${minName.split(".")[0]}.min.${minName.split(".")[1]}`,
-      //     () => {
-      //       FINISHEDFILENUM += 1;
-      //       // TODO sync压缩
-      //       if (FILENUM === FINISHEDFILENUM) {
-      //         let targetPathArr = targetPath.split("/");
-      //         let targetFileName = targetPathArr[targetPathArr.length - 1];
-      //         let newTargetPath = targetPath.replace(
-      //           targetPathArr[targetPathArr.length - 1],
-      //           ""
-      //         );
-      //         zipper.zip(targetPath, function(errZip, zipped) {
-      //           if (errZip) throw errZip;
-      //           zipped.compress();
-      //           zipped.save(`${newTargetPath}${targetFileName}.zip`, function(
-      //             errSave
-      //           ) {
-      //               if (errSave) throw errSave;
-      //               eventReply.reply("dragEventReply", true);
-      //               // TODO 打完压缩包后删除目标文件夹
-      //               // rebuildTarget(targetPath, event, true);
-      //             });
-      //         });
-      //       }
-      //     }
-      //   );
-    } else if (stat.isDirectory()) {
-      // read dir...
-      fs.readdir(fPath, function(errDir, files) {
-        if (errDir) throw errDir;
-        for (file of files) {
-          readFPath(path.join(fPath, file), eventReply);
-        }
-      });
-    }
-  });
-}
+// // 读取文件夹
+// function readFPath (fPath, eventReply) {
+//   fs.lstat(fPath, function (errs, stat) {
+//     if (errs) throw errs
+//     if (stat.isFile()) {
+//       FILENUM += 1
+//       //  compressing...
+//       let minNameArrLeng = fPath.split('/').length
+//       let minName = fPath.split('/')[minNameArrLeng - 1]
+//       renderArr.push({
+//         name: minName,
+//         path: `${fPath}`
+//       })
+//       eventReply.reply('filesList', renderArr)
+//       // tinypng api
+//       // tinify
+//       //   .fromFile(path.resolve(fPath))
+//       //   .toFile(
+//       //     `${targetPath}/${minName.split(".")[0]}.min.${minName.split(".")[1]}`,
+//       //     () => {
+//       //       FINISHEDFILENUM += 1;
+//       //       // TODO sync压缩
+//       //       if (FILENUM === FINISHEDFILENUM) {
+//       //         let targetPathArr = targetPath.split("/");
+//       //         let targetFileName = targetPathArr[targetPathArr.length - 1];
+//       //         let newTargetPath = targetPath.replace(
+//       //           targetPathArr[targetPathArr.length - 1],
+//       //           ""
+//       //         );
+//       //         zipper.zip(targetPath, function(errZip, zipped) {
+//       //           if (errZip) throw errZip;
+//       //           zipped.compress();
+//       //           zipped.save(`${newTargetPath}${targetFileName}.zip`, function(
+//       //             errSave
+//       //           ) {
+//       //               if (errSave) throw errSave;
+//       //               eventReply.reply("dragEventReply", true);
+//       //               // TODO 打完压缩包后删除目标文件夹
+//       //               // rebuildTarget(targetPath, event, true);
+//       //             });
+//       //         });
+//       //       }
+//       //     }
+//       //   );
+//     } else if (stat.isDirectory()) {
+//       // read dir...
+//       fs.readdir(fPath, function (errDir, files) {
+//         if (errDir) throw errDir
+//         for (file of files) {
+//           readFPath(path.join(fPath, file), eventReply)
+//         }
+//       })
+//     }
+//   })
+// }
 
-// 重构目标目录
-function compresePic(event) {
-  // comprese image..
-  try {
-    fs.access(targetPath, fs.constants.F_OK, err => {
-      // if there's not a target dir, make it first.
-      if (err) {
-        fs.mkdir(targetPath, () => {
-          rebuildTarget(targetPath, event);
-        });
-      } else {
-        rebuildTarget(targetPath, event);
-      }
-    });
-  } catch (error) {
-    throw error;
-  }
-}
+// // 重构目标目录
+// function compresePic (event) {
+//   // comprese image..
+//   try {
+//     fs.access(targetPath, fs.constants.F_OK, err => {
+//       // if there's not a target dir, make it first.
+//       if (err) {
+//         fs.mkdir(targetPath, () => {
+//           rebuildTarget(targetPath, event)
+//         })
+//       } else {
+//         rebuildTarget(targetPath, event)
+//       }
+//     })
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-// 重构目标文件
-function rebuildTarget(target, event, del) {
-  renderArr = []
-  fs.readdir(target, "", (err, files) => {
-    if (err) throw err;
-    if (files.length > 0) {
-      files.map(file => {
-        fs.unlinkSync(`${target}/${file}`, err => {
-          if (err) throw err;
-        });
-      });
-    }
-    fs.rmdir(target, errs => {
-      if (errs) throw errs;
-      if (!del) {
-        fs.mkdir(target, err => {
-          if (err) throw err;
-          readFPath(sourcePath, event);
-        });
-      }
-    });
-  });
-}
+// // 重构目标文件
+// function rebuildTarget (target, event, del) {
+//   renderArr = []
+//   fs.readdir(target, '', (err, files) => {
+//     if (err) throw err
+//     if (files.length > 0) {
+//       files.map(file => {
+//         fs.unlinkSync(`${target}/${file}`, err => {
+//           if (err) throw err
+//         })
+//       })
+//     }
+//     fs.rmdir(target, errs => {
+//       if (errs) throw errs
+//       if (!del) {
+//         fs.mkdir(target, err => {
+//           if (err) throw err
+//           readFPath(sourcePath, event)
+//         })
+//       }
+//     })
+//   })
+// }
